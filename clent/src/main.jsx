0bdf05b687a2011d445d7eba20e512d67bcbb371
nmnnrb <151563433+nmnnrb@users.noreply.github.com>
2024-12-26 14:19:52 +0530

    final

--- clent/src/main.jsx
+++ clent/src/main.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+
 import { BrowserRouter } from 'react-router-dom'
  
 
-import { ChatProvider } from './context/ChatProvider' // Correct import path for ChatProvider
-
+import { ChatProvider } from './Context/ChatProvider' 
 createRoot(document.getElementById('root')).render(
 
 
